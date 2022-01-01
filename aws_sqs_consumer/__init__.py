da216e7f0c25a754613cfe880677db095f80f469
Navaneesh Kumar <navaneeshkumarb@gmail.com>
2021-12-31 17:39:11 -0800

    Relax boto3 version constraints

--- aws_sqs_consumer/__init__.py
+++ aws_sqs_consumer/__init__.py
@@ -1,6 +1,9 @@
+from pathlib import Path
+from single_source import get_version
+
 from .consumer import Consumer
 from .message import MessageAttributeValue, Message
 from .error import SQSException
 
-__version__ = "0.0.11"
+__version__ = get_version(__name__, Path(__file__).parent.parent)
 __all__ = ["Consumer", "MessageAttributeValue", "Message", "SQSException"]
